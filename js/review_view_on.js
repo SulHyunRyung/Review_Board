5d8279540f0beea5635a59aef50c79a142c5aeaf
S_HyunRyung <tirofiralr@gmail.com>
2023-02-16 14:50:51 +0900

    index.html / main.css 수정 paging 추가

--- js/review_view_on.js
+++ js/review_view_on.js
@@ -1,6 +1,6 @@
 $(function(){
     $('.review_block').click(function(){
-        $('.review_place').not($(this).siblings()).slideUp(1000);
+        $('.review_place').not($(this).siblings()).stop().slideUp(1000);
         $(this).siblings('.review_place').stop().slideToggle(1000);
     });
 });
